66fecfd48e839cb2c700dd4203aba6db9c3a8b3c
spikecactus <henry1209k@gmail.com>
2024-02-25 15:52:34 +0800

    4-1 complete

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,31 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    thread_func_args->thread_complete_success = false;
+
+    int wait_to_obtain_sec = thread_func_args->wait_to_obtain_ms / 1000;
+    int wait_to_release_sec = thread_func_args->wait_to_release_ms / 1000;
+
+    sleep(wait_to_obtain_sec);
+
+    int mutex_lock = pthread_mutex_lock(thread_func_args->mutex);
+    if(mutex_lock != 0)
+    {   
+        ERROR_LOG("pthread_mutex_lock failed with %d\n", mutex_lock);
+        return thread_param;
+    }
+
+    sleep(wait_to_release_sec);
+
+    int mutex_unlock = pthread_mutex_unlock(thread_func_args->mutex);
+    if(mutex_unlock != 0)
+    {
+        ERROR_LOG("pthread_mutex_unlock failed with %d\n", mutex_unlock);
+        return thread_param;
+    }
+    
+    thread_func_args->thread_complete_success = true;
     return thread_param;
 }
 
@@ -28,6 +52,30 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+
+
+    struct thread_data* thread_func_args = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if(thread_func_args == NULL)
+    {
+        ERROR_LOG("cannot allocate memory");
+        return false;
+    }
+    
+    thread_func_args->thread = thread;
+    thread_func_args->mutex = mutex;
+    thread_func_args->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_func_args->wait_to_release_ms = wait_to_release_ms;
+    thread_func_args->thread_complete_success = false;
+
+    int rc = pthread_create(thread, NULL, threadfunc, (void*)thread_func_args);
+    if(rc != 0)
+    {
+        ERROR_LOG("cannot create thread");
+
+        free(thread_func_args);
+        return false;
+    }
+
+    return true;
 }
 
